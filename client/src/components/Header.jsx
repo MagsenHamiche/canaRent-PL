8875c74e1933497840b8fc6bbb22dcb5e05a626b
MagsenHamiche <magsen.95@gmail.com>
2024-04-06 00:18:59 -0400

    mise a jour du header et la page profile priver

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -3,8 +3,10 @@ import { FaSearch } from 'react-icons/fa'
 import { Link } from 'react-router-dom'
 import Home from '../pages/Home'
 import About from '../pages/About'
+import { useSelector } from 'react-redux'
 
 export default function Header() {
+    const {currentUser} = useSelector(state => state.user);
   return (
     <header className='bg-slate-200 shadow-md '>
         <div className='flex justify-between items-center max-w-6xl mx-auto p-3'>
@@ -28,8 +30,11 @@ export default function Header() {
             <Link to='/about'>
                 <li className='hidden sm:inline text-slate-700 hover:underline'>About</li>
             </Link>
-            <Link to='/sign-in'>
-                <li className=' text-slate-700 hover:underline'>Sing In</li>
+            <Link to='/profile'>
+                {currentUser ? (
+                    <img src={currentUser.avatar} alt='profile' className=' rounded-full h-7 w-7 object-cover'/>
+                ): <li className=' text-slate-700 hover:underline'> Sing In</li>}
+                
             </Link>
             
         </ul>
