45e48ac8435229ea58ed75aa64a88460a98634c8
MagsenHamiche <magsen.95@gmail.com>
2024-04-08 12:33:51 -0400

    create get user listing api route

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -1,7 +1,9 @@
 
 import bcryptjs from 'bcryptjs'
+import Listing from '../models/listing.model.js'
 import User from '../models/user.model.js'
-import { errorHandler } from '../utils/error.js';
+import { errorHandler } from '../utils/error.js'
+
 
 
 export const  test = (req,res) => {
@@ -53,4 +55,17 @@ export const signOut = async (req, res, next)=>{
     } catch (error) {
         next(error);
     }
-}
+};
+
+export const getUserListing = async (req, res, next)=>{
+    if(req.user.id === req.params.id){
+        try {
+            const listings = await Listing.find({userRef: req.params.id});
+            res.status(200).json(listings)
+        } catch (error) {
+            next(error);
+        }
+    }else{
+        return next(errorHandler(401, 'you can only view your own listing'));
+    }
+};
--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { deletUser, test, updateUser } from "../controllers/user.controller.js";
+import { deletUser, test, updateUser, getUserListing } from "../controllers/user.controller.js";
 import { verifyToken } from "../utils/verifyUser.js";
 
 const router = express.Router();
@@ -9,6 +9,9 @@ router.get('/test',test);
 router.post('/update/:id', verifyToken, updateUser)
 //supprimer un compte perso
 router.delete('/delete/:id', verifyToken, deletUser)
+//acceder a nos publications
+router.get('/listings/:id',verifyToken, getUserListing)
+
 
 
 export default router;
