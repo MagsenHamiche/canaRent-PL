297b093f096a0c0e1e13f2442e3e3f9770ca6dba
MagsenHamiche <magsen.95@gmail.com>
2024-04-08 14:16:47 -0400

    route pour update listing

--- api/routes/listing.route.js
+++ api/routes/listing.route.js
@@ -1,11 +1,14 @@
 import express from 'express';
-import { createListing, deleteListing } from '../controllers/listnig.controller.js';
+import { createListing, deleteListing, updateListing} from '../controllers/listnig.controller.js';
 import { verifyToken } from '../utils/verifyUser.js';
 
 const router = express.Router();
-
+//ajouter un listing
 router.post('/create', verifyToken,createListing);
+//supprimer un listing
+router.delete('/delete/:id', verifyToken,deleteListing);
+//mise a jour
+router.post('/update/:id', verifyToken,updateListing);
 
-router.delete('/delete/:id', verifyToken,deleteListing)
 
 export default router;
