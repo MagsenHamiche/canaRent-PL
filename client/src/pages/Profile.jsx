664fa2e91adfcc637de2fc0ae32f904f3caee323
MagsenHamiche <magsen.95@gmail.com>
2024-04-08 14:04:39 -0400

    complete user deletelisting

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -127,6 +127,23 @@ export  default function Profile() {
     } catch (error) {
       setShowListingError(true);
     }
+  };
+
+  const HandleListingDelete = async (listingId) => {
+    try {
+      const res = await fetch(`/api/listing/delete/${listingId}`,{
+        method: 'DELETE',
+      });
+      const data = await res.json();
+      if(data.success === false){
+        console.log(data.message);
+        return;
+      }
+
+      setUserListing((prev) => prev.filter((listing) => listing._id !== listingId));
+    } catch (error) {
+      console.log(error.message)
+    }
   }
   return (
     <div className='p-3 max-w-lg mx-auto'>
@@ -190,7 +207,7 @@ export  default function Profile() {
             <p>{listing.name}</p>
           </Link>
           <div className="flex flex-col items-center ">
-            <button className='text-red-700 uppercase hover:underline'>Delete</button>
+            <button onClick={()=> HandleListingDelete(listing._id)} className='text-red-700 uppercase hover:underline'>Delete</button>
             <button className='text-green-700 uppercase hover:underline'>Edit</button>
           </div>
         </div>
